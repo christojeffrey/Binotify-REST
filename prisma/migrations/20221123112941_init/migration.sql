cfc364787635566e9692f3ae6e5f07e5858d1725
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-11-26 17:41:49 +0700

    adding restrunner for docker, changing schema to mysql

---
+++ prisma/migrations/20221123112941_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `user_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `isAdmin` BOOLEAN NOT NULL DEFAULT false,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_username_key`(`username`),
+    PRIMARY KEY (`user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Song` (
+    `song_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(64) NOT NULL,
+    `audio_path` VARCHAR(191) NOT NULL,
+    `singer_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`song_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Song` ADD CONSTRAINT `Song_singer_id_fkey` FOREIGN KEY (`singer_id`) REFERENCES `User`(`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ restrunner.sh
@@ -0,0 +1,7 @@
+# enable this when a new package is installed
+# npm install
+
+# enable this when a the database is updated 
+# npx prisma migrate dev --name init
+npx prisma generate
+npm run dev
