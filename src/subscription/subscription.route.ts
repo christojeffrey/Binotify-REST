363870c15cf98bf487de41b8150340d1a00ee97f
addinnabilal <16520342@std.stei.itb.ac.id>
2022-11-28 11:44:12 +0700

    feat: implement get all subcription requests api [done, tested]

--- src/subscription/subscription.route.ts
+++ src/subscription/subscription.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import UserRouter from "../user/user.route";
-import { updateSubscription } from "./subscription.controller";
+import { updateSubscription, getAllSubscriptionRequests } from "./subscription.controller";
 const SubscriptionRouter = express.Router();
 
 // subcription/:id
 UserRouter.patch("/subscription/:creator_id", updateSubscription);
+UserRouter.get("/subscription/getAllSubscriptionRequests", getAllSubscriptionRequests);
 
 export default SubscriptionRouter;
